5315a2fda12b98498805c79db4d332c60cc44197
Kelvin Liang <kelvin9877@gmail.com>
2019-06-17 18:35:25 +0800

    finish select-name

--- 1-select-names.sql
+++ 1-select-names.sql
@@ -9,3 +9,52 @@ WHERE name LIKE '%y'
 SELECT name
 FROM world
 WHERE name LIKE '%x%'
+
+SELECT name
+FROM world
+WHERE name LIKE '%land'
+
+SELECT name
+FROM world
+WHERE name LIKE 'C%ia'
+
+SELECT name
+FROM world
+WHERE name LIKE '%oo%'
+
+SELECT name
+FROM world
+WHERE name LIKE '%a%a%a%'
+
+SELECT name
+FROM world
+WHERE name LIKE '_t%'
+ORDER BY name
+
+SELECT name
+FROM world
+WHERE name LIKE '%o__o%'
+
+SELECT name
+FROM world
+WHERE name LIKE '____'
+
+SELECT name
+FROM world
+WHERE name = capital
+
+SELECT name
+FROM world
+WHERE capital = concat(name, ' city')
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE  CONCAT('%', name, '%')
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE  CONCAT('%', name, '_%')
+
+SELECT name, replace(capital, name, '') as ext
+FROM world
+WHERE capital LIKE  CONCAT('%', name, '_%')
